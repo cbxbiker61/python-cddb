6c128308fc703e73f54f23da06dd51cafe4d6f84
Kelly Anderson <kelly@silka.with-linux.com>
2011-08-24 04:37:33 -0600

    fix github references

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name="Cddb",
 	description="CDDB-Server access from Python",
 	author="Kelly Anderson",
 	author_email="cbxbiker61@gmail.com",
-	url="http://github.com/cbxbiker61/cddb",
+	url="http://github.com/cbxbiker61/python-cddb",
 	version="1.0.0",
 	py_modules=[ "Cddb" ])
 
